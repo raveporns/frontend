866d89e04e08b8784441f6db6b48f4484bdf8aca
ravepornsp <supapanchai_r@su.ac.th>
2025-04-27 18:34:26 +0700

    update keycloak

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'https://displayed-fwd-abroad-zum.trycloudflare.com', // URL ของ Keycloak
+    url: 'http://192.168.1.9/', // URL ของ Keycloak
     realm: 'affiliate',
     clientId: 'affiliate',
     checkLoginIframe: false // ปิดการตรวจสอบ iframe เพื่อลดการใช้ Cookie
