9054746d74fad7bfd0bb17460e889ec0d7a61c6d
raveporns <127003474+raveporns@users.noreply.github.com>
2025-04-28 19:54:48 +0700

    Update keycloak.js

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'http://192.168.1.9/', // URL ของ Keycloak
+    url: 'https://192.168.1.9/', // URL ของ Keycloak
     realm: 'affiliate',
     clientId: 'affiliate',
     checkLoginIframe: false // ปิดการตรวจสอบ iframe เพื่อลดการใช้ Cookie
